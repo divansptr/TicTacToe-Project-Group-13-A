b0a94402ca2b14694570aec9cb4dfe3a0302dcd1
Diva <dvnsputri@gmail.com>
2024-12-20 00:20:59 +0700

    add ai class

--- src/TTT/Seed.java
+++ src/TTT/Seed.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #13
+ * 1- 5026231020- Diva Nesia Putri
+ * 2- 5026231114- Imanuel Dwi Prasetyo
+ * 3- 5026231196- Ni Kadek Adelia Paramita Putri
+ */
+
 package TTT;
 
 import javax.swing.*;
